fc293bd173e1d43ced15a79956515b9c47dfa683
Nikola Stekovic <nikola@konstrukt.rs>
2018-02-28 12:46:39 +0100

    Types and codemirror added

--- src/module/cache/QueryCtrl.ts
+++ src/module/cache/QueryCtrl.ts
@@ -1,14 +1,31 @@
+/// <reference path="../../../typings/modules/codemirror/index.d.ts" />
+
 import {ICacheContainer} from "../../services/container/ICacheContainer";
 import {ICache} from "../../services/cache/ICache";
 import {CacheService} from "../../services/cache/CacheService";
 import {IStateService} from "angular-ui-router";
 import {convertBytes} from "../../common/utils/Utils";
+import * as CodeMirror from 'codemirror';
 
 export class QueryCtrl {
-    static $inject: string[] = ["$state", "cacheService", "container", "cache", "allCacheStats", "$http"];
+    static $inject: string[] = ["$state", "cacheService", "container", "cache", "allCacheStats", "$http", '$timeout'];
+
+    private codeMirror: any;
+    private _tableData: any;
 
     constructor(private $state: IStateService, private cacheService: CacheService, private container: ICacheContainer,
-                private cache: ICache, private allCacheStats: any[], private $http: ng.IHttpService) {
+                private cache: ICache, private allCacheStats: any[], private $http: ng.IHttpService, private $timeout: ng.ITimeoutService) {
+        // Wait for view to be rendered
+        this.$timeout(() => {
+            this.codeMirror = CodeMirror.fromTextArea(document.getElementById('query-json-result'), {
+                lineNumbers: true,
+                mode:  {
+                    name: "javascript",
+                    jsonld: true
+                },
+                readOnly: true
+            });
+        })
     }
 
     currentCacheAvailability(): boolean {
@@ -30,12 +47,18 @@ export class QueryCtrl {
     }
 
     loadQuery(): void {
-        const query = document.querySelector('#query-editor').value;
-        this.$http.post("https://jsonplaceholder.typicode.com/posts", {query: query})
+        const query = angular.element(document.querySelector('#query-editor')).val();
+        // this.$http.post("https://jsonplaceholder.typicode.com/posts", {query: query})
+        this.$http.get("https://jsonplaceholder.typicode.com/users")
             .then((data: any) => {
-                document.querySelector('#query-json-result').value = JSON.stringify(data.data);
+                this._tableData = data.data;
+                this.codeMirror.setValue(JSON.stringify(data.data, null, '\t'));
             }
         )
+    }
+
+    get tableData(): Array<Object>[] {
+        return this._tableData;
     }
 
     refresh(): void {
